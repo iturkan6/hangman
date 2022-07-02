994494088c5e84ee1dbb35cfbae8a6d8642af092
iturkan6 <iturkan6@gmail.com>
2022-07-02 17:11:43 +0400

    complete stage 4 changes

--- stage 4/main.py
+++ stage 4/main.py
@@ -3,6 +3,6 @@ import random
 words = ("python", "java", "swift", "javascript")
 print("H A N G M A N")
 guessed_word = random.choice(words)
-given_word = input(f"Guess the word {guessed_word[0:3]}" + "-"*(len(guessed_word)-3) + ":")
+given_word = input("Guess the word {}{}:".format(guessed_word[0:3], "-" * (len(guessed_word) - 3)))
 
 print("You survived!" if given_word.lower() == guessed_word else "You lost!")
